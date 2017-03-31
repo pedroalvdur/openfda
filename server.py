82196580571400a9b90058cf309a1cfea86efbf7
pedroalvdur <pedroj.98.21@gmail.com>
2017-03-31 20:03:23 +0200

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-import web
-import socketserver
-
-# WEB SERVER
-PORT = 8000
-Handler = web.testHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
